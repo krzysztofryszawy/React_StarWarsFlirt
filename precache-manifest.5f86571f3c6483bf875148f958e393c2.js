8070b824b46113315436603bb3d3526249a35359
Krzysztof Ryszawy <krzysztof.ryszawy@gmail.com>
2018-10-31 10:28:12 +0100

    Updates

---
+++ precache-manifest.5f86571f3c6483bf875148f958e393c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3484a02924f1affef2af270b454b421",
+    "url": "/React_StarWarsFlirt/static/media/asteroids.f3484a02.jpg"
+  },
+  {
+    "revision": "afef2605094e00bf0861",
+    "url": "/React_StarWarsFlirt/static/js/runtime~main.afef2605.js"
+  },
+  {
+    "revision": "c4bfeada696f4b25f4dd",
+    "url": "/React_StarWarsFlirt/static/js/main.c4bfeada.chunk.js"
+  },
+  {
+    "revision": "9bc0645c5055137ae7c3",
+    "url": "/React_StarWarsFlirt/static/js/1.9bc0645c.chunk.js"
+  },
+  {
+    "revision": "c4bfeada696f4b25f4dd",
+    "url": "/React_StarWarsFlirt/static/css/main.c0bbeab1.chunk.css"
+  },
+  {
+    "revision": "ae8a2e03c198b20f28cff08ffe337c47",
+    "url": "/React_StarWarsFlirt/index.html"
+  }
+];
